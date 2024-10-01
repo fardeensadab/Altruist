c5a3d3cb73b8ccbf9b5d555e305159e7823f092c
fardeensadab <fardeensadab@gmail.com>
2024-10-01 19:12:32 +0600

    Routing done in between signin and signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import { LoginForm } from "./Components/LoginForm/LoginForm";
+import { SignupForm } from "./Components/SignupForm/SignupForm";
 import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <LoginForm />;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- Frontend/src/Components/SignupForm/SignupForm.jsx
+++ Frontend/src/Components/SignupForm/SignupForm.jsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
-import "./LoginForm.css";
-import { FaRegUser } from "react-icons/fa";
+import React from "react";
+import "./SignupForm.css";
+import { FaEnvelope, FaRegUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
-import { SignupForm } from "../SignupForm/SignupForm";
 import { Link } from "react-router-dom";
 
-export const LoginForm = () => {
+export const SignupForm = () => {
   return (
     <div className="wrapper">
       <form action="">
-        <h1>Login</h1>
+        <h1>Sign up</h1>
         <div className="input-box">
           <input type="text" placeholder="username" required></input>
           <FaRegUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="email" placeholder="email" required></input>
+          <FaEnvelope className="icon" />
         </div>
         <div className="input-box">
           <input type="password" placeholder="password" required></input>
@@ -20,15 +23,13 @@ export const LoginForm = () => {
         </div>
         <div className="remember-forgot">
           <label>
-            <input type="checkbox"></input>Remember me
+            <input type="checkbox"></input>I agree to the terms & conditions
           </label>
-          <a href="#">Forgot password?</a>
         </div>
         <button type="submit">Login</button>
         <div className="register-link">
           <p>
-            Don't have an account?
-            <Link to="/signup"> Sign up</Link>
+            Already have an account? <Link to="/"> Sign in</Link>
           </p>
         </div>
       </form>
