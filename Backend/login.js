5cdeec493911636593ddd1ababfdfd4d71528aab
areeb <areeb@gmail.com>
2024-10-12 15:06:19 +0600

    Added username to registration

--- Backend/login.js
+++ Backend/login.js
@@ -31,22 +31,23 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 })
 
+// login
 app.post("/", encoder, async function (req, res) {
-    var username = req.body.username;
+    var useremail = req.body.useremail;
     var password = req.body.password;
 
     console.log("POST request received at /");
-    console.log("Username:", req.body.username);
+    console.log("useremail:", req.body.useremail);
     // console.log("Password:", req.body.password);
 
-    connection.query("select * from altruist.testusers where user_name = ?", [username], async function (error, results, fields) {
+    connection.query("select * from altruist.testusers where user_email = ?", [useremail], async function (error, results, fields) {
         if (results.length > 0) {
             const passwordMatch = await bcrypt.compare(password, results[0].user_password);
-            console.log(passwordMatch)
+            // console.log(passwordMatch)
             if (passwordMatch) {
                 res.redirect("/welcome");
             }
-            else{
+            else {
                 res.redirect("/");
             }
         } else {
@@ -66,17 +67,18 @@ app.get("/signup", function (req, res) {
 // Handle signup form submission
 app.post("/signup", encoder, async function (req, res) {
     var username = req.body.username;
+    var useremail = req.body.useremail;
     var password = req.body.password;
     const hash = await bcrypt.hash(password, 11);
 
     // Check if the user already exists
-    connection.query("SELECT * FROM testusers WHERE user_name = ?", [username], function (error, results, fields) {
+    connection.query("SELECT * FROM testusers WHERE user_email = ?", [useremail], function (error, results, fields) {
         if (results.length > 0) {
             // User already exists
             res.send("User already exists. <a href='/signup'>Try Again</a>");
         } else {
             // Insert the new user into the database
-            connection.query("INSERT INTO testusers (user_name, user_password) VALUES (?, ?)", [username, hash], function (error, results, fields) {
+            connection.query("INSERT INTO testusers (user_name, user_email, user_password) VALUES (?, ?, ?)", [username, useremail, hash], function (error, results, fields) {
                 if (error) {
                     res.send("Error occurred while signing up. <a href='/signup'>Try Again</a>");
                 } else {
